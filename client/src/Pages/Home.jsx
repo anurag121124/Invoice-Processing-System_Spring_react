656a4ac7187248ae648322d35bc9cd805a1887c3
Anurag Singh <anuragsingh121124@gmail.com>
2023-07-26 12:16:17 +0530

    Commit

---
+++ client/src/Pages/Home.jsx
@@ -0,0 +1,37 @@
+
+import React from "react";
+import Header from "../components/header";
+import { Box, Button, Typography } from "@mui/material";
+import { useState } from "react";
+import AddInvoice from "../components/AddInvoice";
+
+
+
+const Home = () =>{
+
+const [addInvoice, setAddInvoice] = useState(false);
+
+   const toggleInvoice = ()=>{
+    setAddInvoice(true);
+   } 
+    return(
+    <React.Fragment>
+    <Header/>
+    <Box  style={{ margin: 20}}>
+    <Typography variant="h4">Pending Invoice</Typography>
+     {!addInvoice &&< Button 
+           variant="contained" 
+           style={{marginTop : 20 }}
+           onClick={ () => toggleInvoice()}
+           >Add Invoice
+                    
+           </Button>
+}
+    {addInvoice && <AddInvoice/>}
+    </Box>
+    </React.Fragment>
+
+    )
+}
+
+export default Home;
